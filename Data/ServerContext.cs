03e09baa0fefc938249edd334987cbdad1996726
Yura <you@example.com>
2023-08-04 17:37:50 +0300

    create my personal library for relizing new abstract class to work with http requests

--- Data/ServerContext.cs
+++ Data/ServerContext.cs
@@ -4,33 +4,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using WebChatClientApp.Models;
-using System.Windows.Controls;
+
 
 namespace WebChatClientApp.Data
 {
 
-    // класс, который создает дополнительный слой абстракции для работы с запросами к серверу.
-    // Каждый ViewModel, которому будет необходим доступ к серверу, будет должен создать экземпляр этого класса
+    // Класс, который создает дополнительный слой абстракции для работы с запросами к серверу.
+    // Каждый ViewModel, которому будет необходим доступ к серверу, должен создать экземпляр этого класса
     public class ServerContext<T>
     {
         const string url = "https://localhost:7078";
-        public ServerContext()
-        {
-            
-        }
+        public ServerContext() {}
 
         // Метод для создания запросов к серверу
         // Должен иметь реализацию для всех случаев
-        public async Task CreateRequest(string id)
+        //
+        // Для получения моделей из базы данных
+        // adress: ../Controller/
+        public async void CreateRequest(string controller, GetModel getModel)
         {
-            string apiUrl = $"{url}/api/User/{id}";
+            string apiUrl = $"{url}/api/{controller}";
             try
             {
-                await SendRequestToServer(apiUrl);
+                await SendRequestToServer(apiUrl, getModel);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        public async void CreateRequest(string controller, string id, GetModel getModel)
+        {
+            string apiUrl = $"{url}/api/{controller}/{id}";
+            try
+            {
+                await SendRequestToServer(apiUrl, getModel);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        // Для получения коллекций из базы данных
+        // adress: ../Controller/id
+        public async void CreateRequest(string controller, GetCollectinModel getCollectinModel)
+        {
+            string apiUrl = $"{url}/api/{controller}";
+            try
+            {
+                await SendRequestToServer(apiUrl, getCollectinModel);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        public async void CreateRequest(string controller, string id, GetCollectinModel getCollectinModel)
+        {
+            string apiUrl = $"{url}/api/{controller}/{id}";
+            try
+            {
+                await SendRequestToServer(apiUrl, getCollectinModel);
             }
             catch
             {
@@ -39,12 +78,15 @@ namespace WebChatClientApp.Data
         }
 
         // Делегат, который передает значения модели со стороны сервера из данного класса в тот класс, который его реализует
-        // Позволяет реализующим классас абсрагироваться от http запросов
-        private delegate void CreateModel();
+        // Позволяет реализующим классам абсрагироваться от http запросов
+        public delegate void GetModel(T model);
+        public delegate void GetCollectinModel(ICollection<T> model);
 
         // Метод для получения результов от сервера по запросу
+        // Имеет две реализации для моделей и для коллекций
+        //
         // Должен принимать делегат для записи значений из серверных моделей в модели на стороне клиента
-        private async Task SendRequestToServer(string apiUrl)
+        private async Task SendRequestToServer(string apiUrl, GetModel getModel)
         {
             using (HttpClient httpClient = new HttpClient())
             {
@@ -58,10 +100,39 @@ namespace WebChatClientApp.Data
 
                     string responseBody = await response.Content.ReadAsStringAsync();
                     T result = JsonConvert.DeserializeObject<T>(responseBody);
-                    //UserName.Text = result.Name;
-                    //UserLastName.Text = result.LastName;
-                    //UserPassword.Text = result.Password;
-                    //return result;
+                    getModel(result);
+                }
+                catch (HttpRequestException ex)
+                {
+                    MessageBox.Show("Ошибка при отправке запроса: " + ex.Message);
+                    throw new Exception("Ошибка при отправке запроса: " + ex.Message);
+                }
+                catch (JsonException ex)
+                {
+                    MessageBox.Show("Ошибка при обработке JSON: " + ex.Message);
+                    throw new Exception("Ошибка при обработке JSON: " + ex.Message);
+                }
+            }
+        }
+
+        // Должен принимать делегат для записи значений из серверных коллекций в коллекции на стороне клиента
+        private async Task SendRequestToServer(string apiUrl, GetCollectinModel getCollectionModel)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ICollection<T> result = JsonConvert.DeserializeObject<ICollection<T>>(responseBody);
+                    //foreach ()
+                    getCollectionModel(result);
+
                 }
                 catch (HttpRequestException ex)
                 {
