98f6d6c48cd303001ceb294d884c38940d7c4039
Yura <you@example.com>
2023-08-05 23:26:31 +0300

    include SingalR (library for all time connecting with users on http protocol) - it was so hard....

---
+++ Commands/AsyncRelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WebChatClientApp.Commands
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private readonly Func<bool> _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return !_isExecuting && (_canExecute?.Invoke() ?? true);
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (!_isExecuting)
+            {
+                _isExecuting = true;
+                try
+                {
+                    await _execute();
+                }
+                finally
+                {
+                    _isExecuting = false;
+                }
+            }
+        }
+    }
+
+}
--- ViewModels/UserViewModel.cs
+++ ViewModels/UserViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using WebChatClientApp.Commands;
 using WebChatClientApp.Data;
 using WebChatClientApp.Models;
@@ -16,6 +19,8 @@ namespace WebChatClientApp.ViewModels
     public class UserViewModel : INotifyPropertyChanged
     {
         private ServerContext<UserModel> _context;
+
+        private readonly HubConnection connection;
 
         private UserModel user;
         public UserModel User
@@ -42,7 +47,22 @@ namespace WebChatClientApp.ViewModels
         public UserViewModel()
         {
             _context = new ServerContext<UserModel>();
+
+            // создаем подключение к хабу
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7078/chat")
+                .Build();
+
+            connection.On<string, string>("Receive", OnReceiveMessage);
         }
+
+        private void OnReceiveMessage(string message, string a)
+        {
+            MessageBox.Show(message);
+        }
+
+        // обработчик загрузки окна
+
 
         public void AddUserModel(UserModel model)
         {
@@ -56,7 +76,16 @@ namespace WebChatClientApp.ViewModels
             {
                 return getUser ?? (getUser = new Command(obj =>
                 {
-                    _context.CreateRequest("User", (string)obj, AddUserModel);
+                    try
+                    {
+                        // подключемся к хабу
+                        connection.StartAsync();
+                        MessageBox.Show("Вы вошли в чат");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
                 }));
             }
         }
@@ -73,10 +102,35 @@ namespace WebChatClientApp.ViewModels
             {
                 return getUsers ?? (getUsers = new Command(obj =>
                 {
-                    _context.CreateRequest("User", GetUsers);
+                    SendMessageAsync("asdasd");
                 }));
             }
         }
+
+        private async Task SendMessageAsync(string message)
+        {
+            try
+            {
+                await connection.SendAsync("Send", message, "asd");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка отправки сообщения: {ex.Message}");
+            }
+        }
+
+        //private async void SendMessageAsync(string message)
+        //{
+        //    try
+        //    {
+        //        // отправка сообщения
+        //        await connection.InvokeAsync("SendMessageCliend", "Asdasd");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
