761614af7d1bfac764dcff7b1f9ef05cf5086079
Yura <you@example.com>
2023-08-10 20:52:25 +0300

    create converters (can delete)

---
+++ Converters/ChatMenuConverters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WebChatClientApp.Converters
+{
+    class ActiveConnection : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var connection = (HubConnection)value;
+            if (connection != null)
+                if (connection.State == HubConnectionState.Connected)
+                    return "Green";
+            return "Red";
+
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
